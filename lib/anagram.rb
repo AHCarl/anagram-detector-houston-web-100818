c02235aa1410d327f5907215eb3ced21f5a06566
Alexander Carlin <ahcarlin@gmail.com>
2018-10-25 17:18:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    self.word = word
+  end
+  
+  def match(word_arr)
+    c = word_arr.
+    word_arr.select do |w|
+      w.split.sort == word.split("").sort
+      binding.pry
+    end
+    
+  end
+  
+  
+end
